707277ef14b0a41a13f0b66672af539846c3b345
join <join.eu.pn@gmail.com>
2020-08-15 19:31:07 +0300

    test driven development started

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
 use std::fs;
 use std::error::Error;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
-	Ok(())
-}
-
 pub struct Config {
 	pub query: String,
 	pub filename: String,
@@ -21,4 +15,41 @@ impl Config {
 		let filename = args[2].clone();
 		Ok(Config { query, filename })
 	}
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+	Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+	for line in contents.lines() {
+		if line.contains(query) {
+            results.push(line);
+        }
+    }
+	results
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	
+	#[test]
+	fn bad_result() {
+		let query = "";
+		let contents = "one, two, three";
+		assert_ne!(vec!["safe, fast, productive,"], search(query, contents));
+	}
+	
+	fn good_result() {
+		let query = "";
+		let contents = "safe, fast, productive";
+		assert_eq!(vec!["safe, fast, productive,"], search(query, contents));
+	}
+	
 }
