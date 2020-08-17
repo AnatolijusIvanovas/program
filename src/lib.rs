00714032946add59f65165233ca1940bb0f797bc
join <join.eu.pn@gmail.com>
2020-08-17 15:45:15 +0300

    major update: file and query arguments swaped for usability

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,8 @@ impl Config {
 		if args.len() < 3 {
 			return Err("not enough arguments")
 		}
-		let query = args[1].clone();
-		let filename = args[2].clone();
+		let query = args[2].clone();
+		let filename = args[1].clone();
 		let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 		Ok(Config { query, filename, case_sensitive, })
 	}
